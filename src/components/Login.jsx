56398b788041f3a1ad2b61a7728cd8b34e83d0e8
Bidhu1024 <bidhu1024av@gmail.com>
2023-05-01 19:04:49 +0530

    aa

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material'
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [login, signup] = useState('false')
+  return (
+    <Box sx={{width:'100vw', height:'100vh'}}>
+<Box sx={{width:'50vw', backgroundColor:'yellow'}}>
+
+
+</Box>
+<Box sx={{width:'50vw', backgroundColor:'teal'}}></Box>
+    </Box>
+  )
+}
+
+export default Login
